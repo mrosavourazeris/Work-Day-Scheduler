710de8f3d73cea656df89ed99b86a2714ff15dfa
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2020-12-13 16:36:13 -0600

    creating an array for each time table

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,8 @@ var rightNow = moment().format('LL');
 var theTime = moment().format('LT');
 //taking the current time and rounding it down to the whole hour
 var currentHour = theTime.split(":")[0]
+//creating an array for each time table
+var allHours = ["9","10","11","12","1","2","3","4","5"]
 //Added the current date and time to Work Day Scheduler header 
 currentDay.textContent = rightNow
 currentTime.textContent = theTime

fcca048ad0d33e91bcd68d4559644b0fd846d654
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2020-12-13 16:33:19 -0600

    Edited the date and time format showing on the schedule. Also added borders to time table rows

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,12 @@
-//HTML target variables
+//Global variables grabbing DOM elements
 var currentDay = document.getElementById("currentDay")
-
-
+var currentTime = document.getElementById("currentTime")
+var theDay = moment().format('dddd');  
+var rightNow = moment().format('LL');
+var theTime = moment().format('LT');
 //Date, time variables
 var theDay = moment().format('dddd');  
-var rightNow = moment().format("MMMM Do YYYY, h:mm:ss a")
+var rightNow = moment().format('LL');
 
 //Added the current date and time to Work Day Scheduler header 
 currentDay.textContent = rightNow

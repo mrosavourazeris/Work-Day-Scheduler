beeecf3b12a0766b7d6dbb4a07d65e54c380433e
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2020-12-13 16:35:39 -0600

    taking the current time and rounding it down to the whole hour, while saving that into a variable

--- Develop/script.js
+++ Develop/script.js
@@ -4,9 +4,8 @@ var currentTime = document.getElementById("currentTime")
 var theDay = moment().format('dddd');  
 var rightNow = moment().format('LL');
 var theTime = moment().format('LT');
-//Date, time variables
-var theDay = moment().format('dddd');  
-var rightNow = moment().format('LL');
-
+//taking the current time and rounding it down to the whole hour
+var currentHour = theTime.split(":")[0]
 //Added the current date and time to Work Day Scheduler header 
 currentDay.textContent = rightNow
+currentTime.textContent = theTime
